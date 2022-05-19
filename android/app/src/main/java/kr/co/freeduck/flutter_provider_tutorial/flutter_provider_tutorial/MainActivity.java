94274bffed02c78ecc3a5867231abf4a285ffb95
freeduck <freeduc@naver.com>
2022-05-19 11:53:07 +0900

    first commit

---
+++ android/app/src/main/java/kr/co/freeduck/flutter_provider_tutorial/flutter_provider_tutorial/MainActivity.java
@@ -0,0 +1,6 @@
+package kr.co.freeduck.flutter_provider_tutorial.flutter_provider_tutorial;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
